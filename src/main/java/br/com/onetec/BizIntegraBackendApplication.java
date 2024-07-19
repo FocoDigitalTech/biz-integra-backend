31b0ce567dee05ca5fa19c4ef74165bbf2b2b7e2
Danilo <daniloluiz750@gmail.com>
2024-07-19 20:22:15 -0300

    Inicial commit Aplicação

---
+++ src/main/java/br/com/onetec/BizIntegraBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.onetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizIntegraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizIntegraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/onetec/BizIntegraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.onetec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizIntegraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
